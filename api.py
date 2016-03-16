30b61b4b9ba5e61d1e7acb1bbad400bd1adbf01d
battlingRhinos <jpatricksweeney@yahoo.com>
2016-03-16 12:17:08 -0400

    fix: PEP8 online checked

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-"""api.py - Create and configure the "Rock, Paper, Scissors" API exposing the resources.  This file includes all of the game’s logic."""
-
+"""api.py - Create and configure the Rock, Paper, Scissors
+API exposing the resources.
+This file includes all of the game’s logic."""
 
 import logging
 import endpoints
@@ -15,17 +16,20 @@ from utils import get_by_urlsafe
 
 NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
 GET_GAME_REQUEST = endpoints.ResourceContainer(
-    urlsafe_game_key=messages.StringField(1),)
+    urlsafe_game_key=messages.StringField(1), )
 GET_ALL_GAMES_REQUEST = endpoints.ResourceContainer()
-GET_GAMES_BY_USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),)
+GET_GAMES_BY_USER_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1), )
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email=messages.StringField(2))
 
 MEMCACHE_USER_STATS = 'USER_STATS'
 
+
 @endpoints.api(name='rock_paper_scissors', version='v1')
 class RockPaperScissorsApi(remote.Service):
-    """Game API"""
+    """Rock, Paper, Scissors Game API"""
+
     @endpoints.method(request_message=USER_REQUEST,
                       response_message=StringMessage,
                       path='user',
@@ -35,12 +39,11 @@ class RockPaperScissorsApi(remote.Service):
         """Create a User. Requires a unique username"""
         if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
-                    'A User with that name already exists!')
+                'A User with that name already exists!')
         user = User(name=request.user_name, email=request.email)
         user.put()
         return StringMessage(message='User {} created!'.format(
-                request.user_name))
-
+            request.user_name))
 
     @endpoints.method(request_message=NEW_GAME_REQUEST,
                       response_message=GameForm,
@@ -48,11 +51,15 @@ class RockPaperScissorsApi(remote.Service):
                       name='new_game',
                       http_method='POST')
     def new_game(self, request):
-        """Creates new game"""
+        """Creates new rock, paper, scissors game and result.
+        The player passes in their name and choice of rock,
+        paper, or scissors, and the virtual player randomly
+        selects a weapon.  The result (win,lose,draw)
+        is determined."""
         user = User.query(User.name == request.user_name).get()
         if not user:
             raise endpoints.NotFoundException(
-                    'A User with that name does not exist!')
+                'A User with that name does not exist!')
         try:
             game = Game.new_game(user.key, request.weapon)
         except ValueError:
@@ -64,41 +71,51 @@ class RockPaperScissorsApi(remote.Service):
         # so it is performed out of sequence.
         taskqueue.add(url='/tasks/cache_user_stats')
 
-        if game.game_result == DRAW : msg = "it's a"
-        elif game.game_result == UNKNOWN : msg = 'game state is'
-        else: msg = 'you'
-        return game.to_form('You selected {}, your opponent selected {} -- {} {}'.format(game.player_weapon,
-                                                                                         game.opponent_weapon,
-                                                                                         msg,
-                                                                                         game.game_result))
-
+        if game.game_result == DRAW:
+            msg = "it's a"
+        elif game.game_result == UNKNOWN:
+            msg = 'game state is'
+        else:
+            msg = 'you'
+        return game.to_form('You selected {}, your '
+                            'opponent selected {} -- '
+                            '{} {}'.format(game.player_weapon,
+                                           game.opponent_weapon,
+                                           msg,
+                                           game.game_result))
 
     @endpoints.method(request_message=GET_GAME_REQUEST,
                       response_message=GameForm,
                       path='game/{urlsafe_game_key}',
-                      name='get_game',
+                      name='get_game_by_key',
                       http_method='GET')
-    def get_game(self, request):
+    def get_game_by_key(self, request):
         """Return one game by urlsafe_game_key."""
+
         game = get_by_urlsafe(request.urlsafe_game_key, Game)
 
         if not game:
             raise endpoints.NotFoundException('Game not found!')
 
         # Add a descriptive message to response.
-        if game.game_result == 'win' : msg = "You won!"
-        elif game.game_result == 'lose' : msg = "Sorry, the virtual player won this round."
-        elif game.game_result == 'draw' : msg = "The game was a draw."
+        if game.game_result == 'win':
+            msg = "You won!"
+        elif game.game_result == 'lose':
+            msg = "Sorry, the virtual player won this round."
+        elif game.game_result == 'draw':
+            msg = "The game was a draw."
         else:
-            msg = "Unknown result.  Perhaps you chose a weapon we don't know about."
+            msg = "Unknown result.  Perhaps you chose a weapon we don't " \
+                  "know about."
 
         u_key = game.user
         user = u_key.get()
-        return game.to_form('{} chose {} and virtual player chose {}. {}'.format(user.name,
-                                                                                 game.player_weapon,
-                                                                                 game.opponent_weapon,
-                                                                                 msg))
-
+        return game.to_form('{} chose {} and the '
+                            'virtual player chose '
+                            '{}. {}'.format(user.name,
+                                            game.player_weapon,
+                                            game.opponent_weapon,
+                                            msg))
 
     @endpoints.method(request_message=GET_ALL_GAMES_REQUEST,
                       response_message=GameForms,
@@ -109,7 +126,6 @@ class RockPaperScissorsApi(remote.Service):
         """Return all game results."""
         return GameForms(items=[game.to_form('N/A') for game in Game.query()])
 
-
     @endpoints.method(request_message=GET_GAMES_BY_USER_REQUEST,
                       response_message=GameForms,
                       path='games/user/{user_name}',
@@ -117,29 +133,35 @@ class RockPaperScissorsApi(remote.Service):
                       http_method='GET')
     def get_game(self, request):
         """Return a user's game history."""
+
         user = User.query(User.name == request.user_name).get()
         if not user:
             raise endpoints.NotFoundException(
-                    'A User with that name does not exist!')
+                'A User with that name does not exist!')
 
         games = Game.query(Game.user == user.key)
         return GameForms(items=[game.to_form('N/A') for game in games])
-
 
     @endpoints.method(response_message=StringMessage,
                       path='games/cache_user_stats',
                       name='get_user_stats',
                       http_method='GET')
     def get_user_stats(self, request):
-        """Get the cached stats for user"""
-        return StringMessage(message=memcache.get(MEMCACHE_USER_STATS) or 'Nothing found in memcache')
-
+        """Get the cached stats for all users."""
+        return StringMessage(message=memcache.get(MEMCACHE_USER_STATS) or
+                                     'Nothing found in memcache')
 
     @staticmethod
     def _cache_user_stats():
-        """Populates memcache with the number of wins per user"""
+        """Populates memcache with each NEW GAME. Stats include wins,
+        losses, ties, and unknown (if front-end send a weapon we don't
+        know about, this is considered unknown).  Not really an efficient
+        use of memcache but this is for project demonstration purposes mainly.
+        """
+
         users = User.query()
         msg = ''
+        # Build memcache string that details stats for each player.
         for user in users:
             games = Game.query(Game.user == user.key)
             wins = losses = ties = unknown = 0
@@ -153,24 +175,22 @@ class RockPaperScissorsApi(remote.Service):
                 else:
                     unknown += 1
 
-            str1 = "win" if wins == 1 else "wins"
-            str2 = "loss" if losses == 1 else "losses"
-            str3 = "draw" if ties == 1 else "tie games"
-            str4 = "result" if unknown == 1 else "results"
             one_player = "{} has {} {}, {} {}, {} {}, " \
-                   "and {} unknown {}.  ".format(user.name,
-                                                      wins,
-                                                      str1,
-                                                      losses,
-                                                      str2,
-                                                      ties,
-                                                      str3,
-                                                      unknown,
-                                                      str4
-                                                 )
+                         "and {} " \
+                         "unknown {}.  "\
+                .format(user.name,
+                        wins,
+                        "win" if wins == 1 else "wins",
+                        losses,
+                        "loss" if losses == 1 else "losses",
+                        ties,
+                        "draw" if ties == 1 else "tie games",
+                        unknown,
+                        "result" if unknown == 1 else "results"
+                        )
+
             msg += one_player
 
         memcache.set(MEMCACHE_USER_STATS, msg)
-
 
 api = endpoints.api_server([RockPaperScissorsApi])
